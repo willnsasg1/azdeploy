431f88feb4c4d8af011500029a919d6db1ace11d
willnsasg1 <130159202+willnsasg1@users.noreply.github.com>
2026-02-07 18:09:28 -0600

    Add files via upload

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+// prisma.config.ts
+import 'dotenv/config';
+import path from 'node:path';
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  schema: path.join('prisma', 'schema.prisma'),
+  datasource: {
+    url: process.env.DATABASE_URL || '',
+  },
+});
